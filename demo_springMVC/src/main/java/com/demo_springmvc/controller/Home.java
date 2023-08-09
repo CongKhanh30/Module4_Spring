1638affc19906c58adf4d5db244c89bdc9a53730
CongKhanh30 <khanhnc45@wru.vn>
2023-08-09 22:24:57 +0700

    update - 22:24 09/08

--- demo_springMVC/src/main/java/com/demo_springmvc/controller/Home.java
+++ demo_springMVC/src/main/java/com/demo_springmvc/controller/Home.java
@@ -1,4 +1,13 @@
 package com.demo_springmvc.controller;
 
+import com.demo_springmvc.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class Home {
+    @Autowired
+    ProductService productService;
+
+
 }
--- demo_springMVC/src/main/java/com/demo_springmvc/service/ProductService.java
+++ demo_springMVC/src/main/java/com/demo_springmvc/service/ProductService.java
@@ -1,5 +1,32 @@
 package com.demo_springmvc.service;
 
-public class ProductService {
+import com.demo_springmvc.model.Product;
 
+import java.util.ArrayList;
+
+public class ProductService {
+    public ArrayList<Product> products;
+    public ProductService(){
+        products = new ArrayList<>();
+        products.add(new Product(1, "Vanh", 500000, "https://nguoinoitieng.tv/images/nnt/104/0/bhiy.jpg"));
+        products.add(new Product(2, "Amee", 500000, "https://phunuvietnam.mediacdn.vn/179072216278405120/2021/12/3/amee202102-1638541792005741900734.jpg"));
+        products.add(new Product(3, "Myra Trần", 500000, "https://images2.thanhnien.vn/528068263637045248/2023/3/16/edit-myra-tran-9-16789572494021490104536.png"));
+        products.add(new Product(4, "Juky San", 500000, "https://images2.thanhnien.vn/Uploaded/hoangnam/2022_02_04/juky-san-1-7342.jpg"));
+    }
+
+    public Product findById(int id){
+        for (Product p : products) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void edit (Product product1){
+        Product product = findById(product1.getId());
+        product.setName(product1.getName());
+        product.setImg(product1.getImg());
+        product.setPrice(product1.getPrice());
+    }
 }
